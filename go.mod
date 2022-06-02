559326fba9a3032441d5e36f1f1c3b53f6eb9349
lockade <jeffersondias-@hotmail.com>
2022-06-02 16:06:55 -0300

    iniciando front em vue-spa

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -12,11 +13,17 @@ import (
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
+
 	router.HandleFunc("/palavra", palavra.GetAllPalavra).Methods("GET")
 	router.HandleFunc("/palavra", palavra.CreatePalavra).Methods("POST")
 	router.HandleFunc("/palavra/{id}", palavra.GetOnePalavra).Methods("GET")
 	router.HandleFunc("/palavra/{id}", palavra.UpdatePalavra).Methods("PATCH")
 	router.HandleFunc("/palavra/{id}", palavra.DeletePalavra).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":9090", router))
+	// corsObj := handlers.AllowedOrigins([]string{"*"})
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	log.Fatal(http.ListenAndServe(":9090", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
 }
